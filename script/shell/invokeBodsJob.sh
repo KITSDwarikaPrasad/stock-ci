2bccb415c07193f20f3a9da03065a60be674ca95
KITSDwarikaPrasad <dwarika.prasad@kingfisher.com>
2017-04-07 16:54:36 +0100

    bods_play.yaml

--- script/shell/invokeBodsJob.sh
+++ script/shell/invokeBodsJob.sh
@@ -3,7 +3,7 @@
 sh ~/.bashrc
 cd ../../src/main/ansible
 pwd
-ansible-playbook -i hosts/staging bods_play.yml -e "moduleName=win_shell command=JOB_SAPR3_MicroservicenMBODS_STOCK.bat chdirTo=D:\\BODSSHARE"
+ansible-playbook -i hosts/staging bods_play.yaml -e "moduleName=win_shell command=JOB_SAPR3_MicroservicenMBODS_STOCK.bat chdirTo=D:\\BODSSHARE"
 #python --version
 #ansible --version
 #echo $PATH
