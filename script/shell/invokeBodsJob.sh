8f87498f751d8fa9e61b6ac63ecf080e00c6650f
prasad01 <prasad01@GBKTBQH001L2904.kits.kfplc.com>
2017-04-21 14:52:38 +0100

    vault password file

--- script/shell/invokeBodsJob.sh
+++ script/shell/invokeBodsJob.sh
@@ -3,7 +3,7 @@
 sh ~/.bashrc
 cd ../../src/main/ansible
 pwd
-ansible-playbook -i hosts/staging bods_play.yaml -e "moduleName=win_shell command=JOB_SAPR3_MicroservicenMBODS_STOCK.bat chdirTo=D:\\BODSSHARE"
+ansible-playbook -i hosts/staging bods_play.yaml -e "moduleName=win_shell command=JOB_SAPR3_MicroservicenMBODS_STOCK.bat chdirTo=D:\\BODSSHARE" --vault-password-file=~/doNotDelete/.vault_pass
 #python --version
 #ansible --version
 #echo $PATH
