c54abc73425f50975b01c41e302acdaa4a519437
Prasad <prasad01@kits.kfplc.com>
2017-04-24 20:55:32 +0100

    sort 5

--- script/shell/invokeCsvSort.sh
+++ script/shell/invokeCsvSort.sh
@@ -4,7 +4,7 @@ sh ~/.bashrc
 cd ../../src/main/ansible
 pwd
 #ansible-playbook -i hosts/staging jenkins_play.yaml --vault-password-file=~/doNotDelete/.vault_pass
-ansible-playbook -i hosts/staging bods_play.yaml -e "source=SAPR3toStockAPI.csv destination=SAPR3toStockAPI.csv_Actual chdirTo=/BODSSHARE/UKBQ/DSOUT/StockService/" -vvvv
+ansible-playbook -i hosts/staging local_play.yaml -e "source=SAPR3toStockAPI.csv destination=SAPR3toStockAPI.csv_Actual chdirTo=/BODSSHARE/UKBQ/DSOUT/StockService/" -vvvv
 #python --version
 #ansible --version
 #echo $PATH
