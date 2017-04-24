da423fa4f775aeb29e512afd3fd41c9f7a479713
Prasad <prasad01@kits.kfplc.com>
2017-04-24 12:33:18 +0100

    testUtil in test package

---
+++ src/test/java/com/kfplc/ci/test/datafeed/util/UnprocessedLogsTester.java
@@ -0,0 +1,33 @@
+package com.kfplc.ci.test.datafeed.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+import com.kfplc.ci.datafeed.util.ConfigReader;
+
+public class UnprocessedLogsTester {
+	public static void assertIfContains(String inputRow) throws IOException {
+		String logFilePath = ConfigReader.getProperty("UNPROCESSED_LOG_FILE_PATH");
+		boolean found= false;
+		ArrayList<String> matchingLogLines= new ArrayList<String>();
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(logFilePath))) {
+			String logLine = null;
+			while( (logLine = bufferedReader.readLine() ) != null) {
+				if(logLine.contains(inputRow)) {
+					found = true;
+					matchingLogLines.add(logLine);
+				}
+					
+					
+			}
+		}
+		assertTrue("log file content: "+ matchingLogLines, found == false);
+		assertTrue("Input Data not found in Unprocessed logs", found == true);
+		
+	}
+}
