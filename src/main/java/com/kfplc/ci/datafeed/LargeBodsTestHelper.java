1fca9c0c05dfdcd20d1d6df39bcd72c8daa88743
dprasad <dwarika.prasad@kingfisher.com>
2017-05-17 19:59:57 +0100

    polling duration for large file

--- src/main/java/com/kfplc/ci/datafeed/LargeBodsTestHelper.java
+++ src/main/java/com/kfplc/ci/datafeed/LargeBodsTestHelper.java
@@ -31,12 +31,14 @@ public class LargeBodsTestHelper {
 	static String userDir = System.getProperty("user.dir");
 	static String unprocessedLogPath = ConfigReader.getProperty("UNPROCESSED_LOG_FILE_PATH");
 	
+	static int count = 0;
 	/**
 	 * The method to be invoked before running the assert statements  for large BODS Job test, all the operation to be done before the actual test
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
 	public static long invokeLargeBODSJob() throws IOException, InterruptedException {
+		System.out.println("Executing large BODS Job.");
 		String csvFilePath = directory + fileName ;
 		System.out.println("-----------> invoking BODS Job ");
 		long startTime = System.currentTimeMillis();
@@ -57,8 +59,8 @@ public class LargeBodsTestHelper {
 	private static void pollTheFileLargeJob(String strFilePath) throws IOException, InterruptedException {
 
 		boolean fileArrived = false;
-		long pollingDuration = Long.parseLong( ConfigReader.getProperty("POLLING_DURATION_SECONDS") ) * 1000;
-		long pollingInterval = Long.parseLong( ConfigReader.getProperty("POLLING_INTERVAL_SECONDS") ) * 1000;
+		long pollingDuration = Long.parseLong( ConfigReader.getProperty("POLLING_DURATION_SECONDS_LARGE") ) * 1000;
+		long pollingInterval = Long.parseLong( ConfigReader.getProperty("POLLING_INTERVAL_SECONDS_LARGE") ) * 1000;
 		System.out.println("----------> Started Polling for the csv file "+strFilePath +" , with polling interval(in milliSeconds) ="+ pollingInterval);
 		long endTimeSeconds= System.currentTimeMillis() + pollingDuration;
 		Path filePath = Paths.get(strFilePath);
@@ -154,36 +156,28 @@ public class LargeBodsTestHelper {
 	}
 	
 	public static void prepareLargeInputFile() throws SQLException, IOException {
-		
-		 Connection connection = null;
-		 PreparedStatement preparedStatementBQ = null;
-		 ResultSet resultSetBQ = null;
-		try {
-		String sqlQueryBQCd = "select BQCODE from MBODS."+ ConfigReader.getProperty("TBL_EFFECTIVE_ARTICLE")+" where ROWNUM =1";
-		connection = WMBConnection.getConnection();
-		preparedStatementBQ = connection.prepareStatement(sqlQueryBQCd);
-		resultSetBQ = preparedStatementBQ.executeQuery();
-//		final String validBQCode = null;
-//		if(resultSetBQ != null &&) {
-		 resultSetBQ.next();
-		final String validBQCode = (resultSetBQ != null && resultSetBQ.next())? resultSetBQ.getString(1) : "25694956";
-//		}
+		System.out.println("-----------> Preparing input data for large datafeed job test from production like file.");
+		Connection connection = null;
+		PreparedStatement preparedStatementBQ = null;
+		ResultSet resultSetBQ = null;
+		String validBQCode = "25694956";
+
 		Path path = Paths.get(ConfigReader.getProperty("INPUT_FILE_PATH"));
 		Path bkpPath = Paths.get(ConfigReader.getProperty("INPUT_FILE_PATH") + "BKP");
 		Files.move(path, bkpPath);
-		if(Files.notExists(path)) {
+		if (Files.notExists(path)) {
 			Files.createFile(path);
 		}
-		Files.lines(bkpPath)
-		  .map(line-> new StringBuilder(line).replace(15, 23, validBQCode).toString()).parallel()
-		  .forEach(line -> {
-			try {
-				Files.write(path, line.concat("\n").getBytes(),StandardOpenOption.APPEND);
-				System.out.print(".");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+		try {
+			String sqlQueryBQCd = "select BQCODE from MBODS." + ConfigReader.getProperty("TBL_EFFECTIVE_ARTICLE")
+					+ " where ROWNUM =1";
+			connection = WMBConnection.getConnection();
+			preparedStatementBQ = connection.prepareStatement(sqlQueryBQCd);
+			resultSetBQ = preparedStatementBQ.executeQuery();
+			// final String validBQCode = null;
+			// if(resultSetBQ != null &&) {
+			resultSetBQ.next();
+			validBQCode = (resultSetBQ != null && resultSetBQ.next()) ? resultSetBQ.getString(1) : "25694956";
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -191,10 +185,29 @@ public class LargeBodsTestHelper {
 			WMBConnection.closePreparedStatement(preparedStatementBQ);
 			WMBConnection.closeConnection(connection);
 		}
+		final String validBQCode1 = validBQCode;
+		
+		Files.lines(bkpPath).map(line -> new StringBuilder(line).replace(15, 23, validBQCode1).toString()).parallel()
+				.forEach(line -> {
+					try {
+						updateCount();
+						Files.write(path, line.concat("\n").getBytes(), StandardOpenOption.APPEND);
+						if(count % 1000 == 0){
+							System.out.print(".");
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
 
 	}
 
+	private static void updateCount() {
+		count++;
+	}
+
 	public static void postJUnitCleanUp(TestCasePosition last) throws IOException {
+		System.out.println("Reverting input files.");
 		Path inputPath = Paths.get(ConfigReader.getProperty("INPUT_FILE_PATH"));
 		Files.delete(inputPath);
 		Files.move(Paths.get(ConfigReader.getProperty("INPUT_FILE_PATH") + "BKP"), inputPath);
